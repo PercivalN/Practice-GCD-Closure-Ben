6dab76c37a21af1d6fb36c1eee17626c4595abd3
Percy <percival.ngan@gmail.com>
2019-12-12 00:43:43 -0600

    Added Student model

---
+++ Students/Models/Student.swift
@@ -0,0 +1,15 @@
+//
+//  Student.swift
+//  Students
+//
+//  Created by Percy Ngan on 12/12/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+struct Student: Codable {
+	var name: String
+	var course: String
+
+}
---
+++ Students/Models/StudentController.swift
@@ -0,0 +1,9 @@
+//
+//  StudentController.swift
+//  Students
+//
+//  Created by Percy Ngan on 12/12/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
